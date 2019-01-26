e2232208851edc6bbb3cb5451250026f07009741
zzj <zzj@lanqiao.org>
2019-01-26 11:56:22 +0800

    主页

--- src/main/java/org/lanqiao/phone/controller/CommodityController.java
+++ src/main/java/org/lanqiao/phone/controller/CommodityController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,17 +20,31 @@ public class CommodityController {
 
     @RequestMapping("/chacun")
     public String selectCommodity(String c_name, Model model){
-        System.out.println(c_name);
         List<Commodity> commodityList = commodityService.selectCommidity(c_name);
-        model.addAttribute("commodityList",commodityList);
+        List<Commodity> com = new ArrayList<>();
+        List<List<Commodity>> comm = new ArrayList<>();
+        if(commodityList.size()<5){
+            comm.add(commodityList);
+        }
+        for(int i=1;i<commodityList.size()+1;i++){
+            if(com.size()%5==0&&com.size()!=0){
+                comm.add(com);
+                com.clear();
+                i--;
+            }else {
+                if(i==10){
+                    comm.add(com);
+                }
+                com.add(commodityList.get(i-1));
+            }
+        }
+        model.addAttribute("commodityList",comm);
         return "liebiao";
     }
 
     @RequestMapping("/xiangqing.do")
     public String link(String c_name,Model model){
-        System.out.println(c_name);
         Commodity commodity = commodityService.link(c_name);
-        System.out.println(c_name);
         model.addAttribute("commodity",commodity);
         return "xiangqing";
     }
--- src/test/java/org/lanqiao/phone/PhoneApplicationTests.java
+++ src/test/java/org/lanqiao/phone/PhoneApplicationTests.java
@@ -2,12 +2,42 @@ package org.lanqiao.phone;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lanqiao.phone.pojo.Commodity;
+import org.lanqiao.phone.service.impl.CommodityServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PhoneApplicationTests {
+    @Autowired
+    CommodityServiceImpl commodityService;
+
+    @Test
+    public void selectCommodity(){
+        List<Commodity> commodityList = commodityService.selectCommidity("OPPO");
+        System.out.println(commodityList.size()+"======");
+        List<Commodity> com = new ArrayList<>();
+        List<List<Commodity>> comm = new ArrayList<>();
+        for(int i=1;i<commodityList.size()+1;i++){
+            if(com.size()%5==0&&com.size()!=0){
+                comm.add(com);
+                com.clear();
+                i--;
+            }else {
+                if(i==10){
+                    comm.add(com);
+                }
+                com.add(commodityList.get(i-1));
+            }
+        }
+    }
 
     @Test
     public void contextLoads() {
